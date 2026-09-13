1d060697a64e1a9a902ee58708cf25387c7e80c0
jwoo13 <nachanee12@naver.com>
2026-09-13 21:13:44 +0900

    [#6] Feat: ì7진 및 그림 저장 로직 구현

--- src/main/java/com/jung/daysum/service/DailyRecordService.java
+++ src/main/java/com/jung/daysum/service/DailyRecordService.java
@@ -2,26 +2,52 @@ package com.jung.daysum.service;
 
 import com.jung.daysum.domain.DailyRecord;
 import com.jung.daysum.dto.DailyRecordDto;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 public interface DailyRecordService {
 
-    DailyRecordDto.MoodResponse updateTodayMood(DailyRecordDto.MoodUpdateRequest moodUpdateRequestDto);
+    DailyRecordDto.MoodResponse updateTodayMood(
+            DailyRecordDto.MoodUpdateRequest moodUpdateRequestDto
+    );
 
-    DailyRecordDto.Response findDailyRecordByDate(LocalDate recordDate);
+    DailyRecordDto.PhotoResponse updateTodayPhoto(
+            MultipartFile imageFile
+    ) throws IOException;
 
-    DailyRecordDto.DiaryResponse updateDiary(LocalDate recordDate,
+    void deleteTodayPhoto();
+
+    DailyRecordDto.DrawingResponse updateTodayDrawing(
+            MultipartFile imageFile
+    ) throws IOException;
+
+    void deleteTodayDrawing();
+
+    DailyRecordDto.Response findDailyRecordByDate(
+            LocalDate recordDate
+    );
+
+    DailyRecordDto.DiaryResponse updateDiary(
+            LocalDate recordDate,
             DailyRecordDto.DiaryUpdateRequest diaryUpdateRequestDto
     );
 
-    void deleteDiary(LocalDate recordDate);
+    void deleteDiary(
+            LocalDate recordDate
+    );
 
-    DailyRecordDto.DiaryShareResponse updateDiaryShare(LocalDate recordDate,
+    DailyRecordDto.DiaryShareResponse updateDiaryShare(
+            LocalDate recordDate,
             DailyRecordDto.DiaryShareRequest diaryShareRequestDto
     );
 
+
     // ========== 유틸성 메소드 ========== //
 
-    DailyRecord findDailyRecord(Long userId, LocalDate recordDate);
+    DailyRecord findDailyRecord(
+            Long userId,
+            LocalDate recordDate
+    );
 }
